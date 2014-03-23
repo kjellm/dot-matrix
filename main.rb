f30c94aa1815a6967d0d87f3ad479f24d00982b2
Kjell-Magne Øierud <kjellm@oierud.net>
2014-03-23 13:37:32 +0100

    update

--- main.rb
+++ main.rb
@@ -1,11 +1,30 @@
 require 'minitest/autorun'
+require 'date'
+require 'rack/test'
+require 'sinatra/base'
 
 module DotMatrix
+
+  UnknownAttributeError = Class.new ArgumentError
+
+  ##########################################################################
+
+  class Repository
+    
+    def initialize
+      @repos = { Consultant => ConsultantRepository.new }
+    end
+
+    def for(klass)
+      @repos.fetch klass
+    end
+  end
 
   class ConsultantRepository
 
     def initialize
       @objects = []
+      @id = -1
     end
 
     def count
@@ -16,13 +35,22 @@ module DotMatrix
       @objects.first
     end
 
+    def find(id)
+      @objects[id]
+    end
+
     def save obj
+      @id += 1
+      obj.id = @id
       @objects << obj
     end
   end
 
+  ##########################################################################
+
   class Consultant
-    attr_accessor :name
+
+    attr_accessor :id, :name
    
     def initialize values={}
       values.each_pair do |key, value|
@@ -35,7 +63,10 @@ module DotMatrix
     end
   end
 
+  ##########################################################################
+
   class ConsultantForm
+
     attr_accessor :name
 
     def initialize values={}
@@ -51,19 +82,29 @@ module DotMatrix
     def valid?
       true
     end
-
   end
 
-  class Repository
-    
-    def initialize
-      @repos = { Consultant => ConsultantRepository.new }
+  class AssignmentForm
+
+    attr_accessor :starts_at, :ends_at
+    attr_accessor :project
+
+    def initialize values={}
+      values.each_pair do |key, value|
+        if respond_to? "#{key}="
+          send "#{key}=", value
+        else
+          raise UnknownAttributeError, key
+        end
+      end
     end
 
-    def for(klass)
-      @repos.fetch klass
+    def valid?
+      true
     end
   end
+
+  ##########################################################################
 
   class CreateConsultant
 
@@ -79,6 +120,89 @@ module DotMatrix
       obj
     end
   end
+
+  class AssignConsultant
+
+    def initialize consultant:, form:, repo:
+      @form = form
+      @repo = repo
+    end
+
+    def run!
+    end
+  end
+
+  ##########################################################################
+
+  class Server < Sinatra::Base
+    
+    class ParameterMissingError < StandardError
+      def initialize(key)
+        @key = key
+      end
+
+      def to_s
+        %Q{Request did not provide "#{@key}"}
+      end
+    end
+
+    def initialize(*args)
+      super(*args)
+      @repo = Repository.new
+    end
+
+    get "/" do
+    end
+
+    get "/consultant/:id" do |id|
+      id = id.to_i
+      consultant = @repo.for(Consultant).find(id)
+      "<h2>#{consultant.name}</h2>"
+    end
+
+    post "/consultant" do
+      form = ConsultantForm.new name: extract!(:name)
+      use_case = CreateConsultant.new form, @repo.for(Consultant)
+      consultant = use_case.run!
+      redirect to("/consultant/#{consultant.id}")
+    end
+
+    helpers do
+      def extract!(key)
+        value = params.fetch(key.to_s) do
+          raise ParameterMissingError, key
+        end
+        value
+      end
+    end
+
+  end
+
+  ##########################################################################
+
+  class ServerTest < MiniTest::Unit::TestCase
+    include Rack::Test::Methods
+
+    def app
+      Server
+    end
+
+    def test_returns_a_200_for_root
+      get '/'
+      assert_equal 200, last_response.status
+    end
+
+    def test_create_new_consultant
+      post '/consultant', { name: "Foo Bar" }
+      follow_redirect!
+      assert_equal 200, last_response.status
+      assert_match /Foo Bar/, last_response.body
+    end
+
+  end
+
+
+  ##########################################################################
 
   class CreateConsultantTest < MiniTest::Unit::TestCase
 
@@ -96,7 +220,24 @@ module DotMatrix
       assert_equal 1, @repo.count
       assert_equal consultant, @repo.first
     end
+  end  
   
+  class AssignConsultantTest < MiniTest::Unit::TestCase
+
+    def setup
+      @consultant = Consultant.new name: "Foo"
+      r = Repository.new
+      @repo = r.for(Consultant)
+    end
+
+    def test_assigning_a_consultant_to_a_project
+      form = AssignmentForm.new starts_at: Date.new(2014, 1, 1), ends_at: Date.new(2014, 2, 1), project: "Bar"
+      use_case = AssignConsultant.new consultant: @consultant, form: form, repo: nil
+      consultant = use_case.run!
+      
+      refute_nil consultant
+      assert_equal 1, consultant.assignments.count
+    end
   end
 end
 
